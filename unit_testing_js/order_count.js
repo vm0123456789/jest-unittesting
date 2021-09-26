1c0d9e14cb8ef48c7b03e109643f1e9748e568b4
Viktor Mishyn <viktormishyn@gmail.com>
2021-09-26 15:50:41 +0300

    Add order_count.js code exemple

---
+++ unit_testing_js/order_count.js
@@ -0,0 +1,20 @@
+const someOrder = {
+  items: [
+    { name: "Dragon food", price: 8, quantity: 8 },
+    { name: "Dragon cage (small)", price: 800, quantity: 2 },
+    { name: "Shipping", price: 40, shipping: true },
+  ],
+};
+
+const orderTotal = (order) => {
+  const totalItems = order.items
+    .filter((x) => !x.shipping)
+    .reduce((accum, cur) => accum + cur.price * cur.quantity, 0);
+  const shippingItem = order.items.find((x) => !!x.shipping);
+  // !! - short way to cast a variable to be a boolean (true or false) value.
+  const shipping = totalItems > 1000 ? 0 : shippingItem.price;
+  return totalItems + shipping;
+};
+
+result = orderTotal(someOrder);
+console.log(result);
